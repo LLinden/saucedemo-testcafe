38980de64b9ae0a9cd0cc82137360ec098e79794
LLinden <lucasflinden@gmail.com>
2025-09-03 15:12:30 -0300

    mudanca para page object model

---
+++ pages/login.page.js
@@ -0,0 +1,19 @@
+import { Selector, t } from "testcafe";
+
+class LoginPage {
+  constructor() {
+    this.inputUsuario = Selector("#user-name");
+    this.inputSenha = Selector("#password");
+    this.botaoLoginbotaoLogin = Selector("#login-button");
+  }
+
+  async loginSucesso(usuario, senha) {
+    await t
+      // paste true para digitar texto todo de uma vez e não letra por letra
+      .typeText(this.inputUsuario, usuario, { paste: true })
+      .typeText(this.inputSenha, senha, { paste: true })
+      .click(this.botaoLogin);
+  }
+}
+
+export default new LoginPage();
