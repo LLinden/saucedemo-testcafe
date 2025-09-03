38a0719072bb7590a06b5e8a0b456f85574863fc
LLinden <lucasflinden@gmail.com>
2025-09-03 16:30:20 -0300

    criacao do cenario de usuario invalido

--- pages/login.page.js
+++ pages/login.page.js
@@ -5,14 +5,19 @@ class LoginPage {
     this.inputUsuario = Selector("#user-name");
     this.inputSenha = Selector("#password");
     this.botaoLogin = Selector("#login-button");
+    this.mensagemErro = Selector('[data-test="error"]');
   }
 
-  async loginSucesso(usuario, senha) {
+  async login(usuario, senha) {
     await t
       // paste true para digitar texto todo de uma vez e não letra por letra
       .typeText(this.inputUsuario, usuario, { paste: true })
       .typeText(this.inputSenha, senha, { paste: true })
       .click(this.botaoLogin);
+  }
+
+  async mensagemErroLogin() {
+    return this.mensagemErro.innerText;
   }
 }
 
--- tests/login.test.js
+++ tests/login.test.js
@@ -4,11 +4,25 @@ import dotenv from "dotenv";
 // carrega variáveis do .env
 dotenv.config();
 
-fixture("Teste de Login").page(process.env.URL_BASE);
+fixture("Testes de Login").page(process.env.URL_BASE);
 
 test("Login com sucesso", async (t) => {
-  await loginPage.loginSucesso(process.env.USUARIO_VALIDO, process.env.SENHA_USER_VALIDO);
+  await loginPage.login(
+    process.env.USUARIO_VALIDO,
+    process.env.SENHA_USER_VALIDO
+  );
   await t
     .expect(await t.eval(() => window.location.href))
     .eql(process.env.URL_POS_LOGIN);
+});
+
+test("Login com usuário inválido", async (t) => {
+  const mensagemErroEsperada =
+    "Epic sadface: Username and password do not match any user in this service";
+  await loginPage.login(
+    process.env.USUARIO_INVALIDO,
+    process.env.SENHA_USER_VALIDO
+  );
+  const mensagemErroObtida = await loginPage.mensagemErro();
+  await t.expect(mensagemErroObtida).eql(mensagemErroEsperada);
 });
